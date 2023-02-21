e4aa074ed776137cf96340b95c91d59e72a1d890
JAVOHIRXON <kamongaron.j@gmail.com>
2023-02-21 18:33:53 +0500

    02

--- is_odd.py
+++ is_odd.py
@@ -1,3 +1,4 @@
-#Create a variable "number" and assign it a integer value.
-
+#Create a variable "number" and assign it a integer valu
+number = 45
 #Print 1 if the number is odd, otherwise print 0.
+print((number + 1)%2)
