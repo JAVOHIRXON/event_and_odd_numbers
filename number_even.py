4a6dadb3632a958a119d24b638cfa3efaba3d9fb
JAVOHIRXON <kamongaron.j@gmail.com>
2023-02-22 15:01:32 +0500

    03

--- number_even.py
+++ number_even.py
@@ -3,9 +3,13 @@
 var_int = 2543
 #Print the number of even digits in the variable "var_int".
 x1 = var_int%10
+
 var_int=var_int//10
 x2 = var_int%10
+
 var_int=var_int//10
 x3=var_int%10
+ 
+ 
 x4=var_int//10
-print(x4%2+x3%2+x2%2+x1%2)
+print((x4+1)%2+(x3+1)%2+(x2+1)%2+(x1+1)%2)
