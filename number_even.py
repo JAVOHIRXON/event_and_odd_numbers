eebeeea0deff49aefc8e424ddc46b6833df0e4d4
JAVOHIRXON <kamongaron.j@gmail.com>
2023-02-22 14:51:12 +0500

    03

--- number_even.py
+++ number_even.py
@@ -2,4 +2,10 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 var_int = 2543
 #Print the number of even digits in the variable "var_int".
-print(var_int%100)
+x1 = var_int%10
+var_int=var_int//10
+x2 = var_int%10
+var_int=var_int//10
+x3=var_int%10
+x4=var_int//10
+print((x4+1)%2+(x3+1)%2+(x2+1)%2+(x1+1)%2)
